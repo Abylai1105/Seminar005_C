eda7c05b7b1e98483028c9e5a9ada7702bb4858a
Abylai <aba.omar@mail.ru>
2022-05-04 15:56:08 +0600

    Task001 new

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,8 +1,8 @@
-﻿/*
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 int [] numbers = new int [8];
 for(int i = 0; i < numbers.Length; i++)
 {
     Console.Write($"Введите число {i + 1}: ");
     numbers [i] = Convert.ToInt32(Console.ReadLine());
 }
-*/
+
