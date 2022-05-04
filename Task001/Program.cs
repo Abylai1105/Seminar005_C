3190f89196f38ae1e4c46d9f7a8cd637d294ba89
Abylai <aba.omar@mail.ru>
2022-05-04 15:52:59 +0600

    task001

---
+++ Task001/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+int [] numbers = new int [8];
+for(int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    numbers [i] = Convert.ToInt32(Console.ReadLine());
+}
+*/
