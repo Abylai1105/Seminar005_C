b1fa413c9b75567fdb97051db7350fe5a502c7ce
Abylai <aba.omar@mail.ru>
2022-05-06 20:37:37 +0600

    Ошибка в 036

--- Domashka036/Program.cs
+++ Domashka036/Program.cs
@@ -2,3 +2,35 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[sizeArray];
+
+void WriteArray(int[] array)
+{
+    for(int index = 0; index < array.Length; index++)
+    {
+        Console.Write(array[index] + " ");
+    }
+    Console.WriteLine();
+}
+void FillArray(int[] array)
+{
+    for(int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(-100, 100);
+    }
+}
+
+FillArray(randomArray);
+WriteArray(randomArray);
+
+int sum = 0;
+int mas = 0;
+for (int index = 0; index < randomArray.Length; index++)
+{
+    while(index + 1)
+    sum += index;
+}
+Console.WriteLine(sum);
